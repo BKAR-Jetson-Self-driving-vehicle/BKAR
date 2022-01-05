5a4ea67fd2894dc5b00e59eb7edc1834c1100baf
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-01-05 05:47:43 -0500

    remake Camera module

--- utils/Camera.py
+++ utils/Camera.py
@@ -15,124 +15,105 @@ import time
 import threading
 
 
-class CamCSI:
-    """
-    """
-    def __init__(self, sensorID=0, sensorMode=3,
-                 height=720, width=1280, fps=60,
-                 flipMode=0, record=False):
+class CSI_Camera:
 
-        self.frame = None
-        self.ret = False
+    def __init__(self):
         self.video_capture = None
+        self.frame = None
+        self.grabbed = False
+        self.read_thread = None
+        self.read_lock = threading.Lock()
+        self.running = False
 
-        self.sensor_id = sensorID
-        self.sensor_mode = sensorMode
-        self.fps = fps
-        self.flip_mode = flipMode
-        self.height = height
-        self.width = width
-        self._gstreamer_pipeline = ""
-
-        self.__record = record
-
-        self.__frames_read = 0
-
-        self.__running = False
-        self.__lock = threading.Lock()
-        self.__thread_capture = None
-        self.__thread_record = None
-
-    def create_gstreamer_pipeline(self):
-        self._gstreamer_pipeline = (
-            "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
-            "video/x-raw(memory:NVMM), "
-            "format=(string)NV12, framerate=(fraction)%d/1 ! "
-            "nvvidconv flip-method=%d ! "
-            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx !"
-            "videoconvert ! "
-            "video/x-raw, format=(string)BGR ! appsink"
-            % (
-                self.sensor_id,
-                self.sensor_mode,
-                self.fps,
-                self.flip_mode,
-                self.width,
-                self.height,
-            )
-        )
-
-    def open(self):
-        # create gstreamer pipeline
-        self.create_gstreamer_pipeline()
-
+    def open(self, gstreamer_pipeline_string):
         try:
             self.video_capture = cv2.VideoCapture(
-                self._gstreamer_pipeline, cv2.CAP_GSTREAMER
+                gstreamer_pipeline_string, cv2.CAP_GSTREAMER
             )
+
         except RuntimeError:
             self.video_capture = None
             print("Unable to open camera")
-            print("Pipeline: " + self.gstreamer_pipeline)
+            print("Pipeline: " + gstreamer_pipeline_string)
             return
-
-        # Grab the first frame to start the video capturing
         self.grabbed, self.frame = self.video_capture.read()
 
     def start(self):
         if self.running:
             print('Video capturing is already running')
             return None
-
-        # create a thread to read the camera image
         if self.video_capture is not None:
-            self.__running = True
-            self.__thread_capture = threading.Thread(target=self.capture)
-            self.__thread_capture.setDaemon = True
-            self.__thread_capture.start()
-        if self.__record:
-            self.__thread_record = threading.Thread(target=self.record)
-            self.__thread_record.setDaemon = True
-            self.__thread_record.start()
+            self.running = True
+            self.read_thread = threading.Thread(target=self.updateCamera)
+            self.read_thread.start()
         return self
 
-    def capture(self):
-        while self.__running:
+    def stop(self):
+        self.running = False
+        self.read_thread.join()
+
+    def updateCamera(self):
+        while self.running:
             try:
-                ret, frame = self.video_capture.read()
-                with self.__lock:
-                    self.ret = ret
+                grabbed, frame = self.video_capture.read()
+                with self.read_lock:
+                    self.grabbed = grabbed
                     self.frame = frame
-                    self.__frames_read += 1
             except RuntimeError:
                 print("Could not read image from camera")
 
-    def getFrame(self):
-        return self.frame
+    def read(self):
+        with self.read_lock:
+            frame = self.frame.copy()
+            grabbed = self.grabbed
+        return grabbed, frame
 
-    def showCapture(self):
-        while True:
-            with self.__lock:
-                cv2.imshow('CSI Camera ' + str(self.sensor_id), self.frame)
-                keyCode = cv2.waitKey(20) & 0xFF
+    def release(self):
+        if self.video_capture is not None:
+            self.video_capture.release()
+            self.video_capture = None
+        if self.read_thread is not None:
+            self.read_thread.join()
 
-                if keyCode == 27:
-                    break
-        cv2.destroyAllWindows()
 
-    def stop(self):
-        self.__running = False
-        self.__thread_capture.join()
-        self.__thread_record.join()
-
-    def release():
+class BKAI_Cameras:
+    """
+    """
+    def __init__(self, sensorID=0, sensorMode=3,
+                 height=720, width=1280, fps=60,
+                 flipMode=0, record=False):
         pass
 
-
-class DualCamera:
-    """
-    """
-    pass
+    def gstreamer_pipeline(
+        sensor_id=0,
+        sensor_mode=2,
+        capture_width=1280,
+        capture_height=720,
+        display_width=1280,
+        display_height=720,
+        framerate=30,
+        flip_method=0,
+    ):
+        return (
+            "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx !"
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+            % (
+                sensor_id,
+                sensor_mode,
+                capture_width,
+                capture_height,
+                framerate,
+                flip_method,
+                display_width,
+                display_height,
+            )
+        )
 
 
 if __name__ == '__main__':
