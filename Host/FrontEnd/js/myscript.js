c0a745f4d8c3a0e65d00394650495e538d3bb2ec
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-01-05 16:40:31 +0700

    updae Script file

--- Host/FrontEnd/js/myscript.js
+++ Host/FrontEnd/js/myscript.js
@@ -1,5 +1,3 @@
-alert("Do mình không phải là Frontend Dev nên có thể xảy ra một chút vấn đề về giao diện, ví dụ như bể layout, responsive. Mong các bạn thông cảm :D")
-
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     alert("Giao diện không phù hợp cho thiết bị di động, vui lòng truy cập vào bằng thiết bị màn hình lớn!");
 }else
