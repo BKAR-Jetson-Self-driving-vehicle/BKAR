20fd1c8b77ec2f7d703e113cf29888719c462d5b
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-01-06 14:07:05 +0700

    update Time

--- Host/FrontEnd/js/myscript.js
+++ Host/FrontEnd/js/myscript.js
@@ -1,6 +1,30 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     alert("Giao diện không phù hợp cho thiết bị di động, vui lòng truy cập vào bằng thiết bị màn hình lớn!");
-}else
-   {
-   //Now include js files
-   }
+} else {
+    //Now include js files
+}
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds();
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    setTimeout(showTime, 1000);
+}
+showTime();
