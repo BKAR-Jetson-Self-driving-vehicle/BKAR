a2df516ec691e070042ed37c591cd722e5c6ffd9
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-01-07 11:59:33 +0700

    update APIs

--- Host/Final-Server/app.py
+++ Host/Final-Server/app.py
@@ -1,7 +1,10 @@
 import json
 from flask import Flask, redirect, request, url_for, render_template
+from flask_restful import Resource, Api
 app = Flask(__name__)
+api = Api(app)
 
+# Views
 @app.route('/')
 def Dashboard():
     return render_template('index.html')
@@ -21,6 +24,21 @@ def configConnection():
 @app.route('/StartConnection')
 def startConnection():
     return 'Start connection'
+
+# API
+class Status(Resource):
+    def get(self):
+        return {'hello': 'api'}
+
+class Control(Resource):
+    pass
+
+class Stream(Resource):
+    pass
+
+api.add_resource(Status, '/Status')
+api.add_resource(Control, '/Control')
+api.add_resource(Control, '/Stream')
 
 if __name__ == '__main__':
     app.run(debug=True)
