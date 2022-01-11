5245122c6f6d9e37cc89973a2f9fe70a46ae3ccb
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-01-11 07:48:32 +0700

    update args for apis

--- Host/Final-Server/app.py
+++ Host/Final-Server/app.py
@@ -4,7 +4,34 @@ from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
-parser = reqparse.RequestParser()
+
+SYSTEM_ARGS_PUT_API = reqparse.RequestParser()
+SYSTEM_ARGS_PUT_API.add_argument('TIMESTAMP', type=int, help='Time send request.')
+SYSTEM_ARGS_PUT_API.add_argument('IP', type=str, help='IP Address of BKAR.')
+SYSTEM_ARGS_PUT_API.add_argument('CONNECTED', type=bool, help='Connect status.')
+SYSTEM_ARGS_PUT_API.add_argument('DISTANCE', type=int, help='Distance traveled.')
+SYSTEM_ARGS_PUT_API.add_argument('VOLTAGE', type=float, help='Battery voltage.')
+SYSTEM_ARGS_PUT_API.add_argument('TRAFFIC_SIGN', type=str, help='Traffic sign just met.')
+SYSTEM_ARGS_PUT_API.add_argument('GEAR', type=str, help='Current gear mode.')
+SYSTEM_ARGS_PUT_API.add_argument('MODE', type=str, help='Current driving mode.')
+
+SENSOR_ARGS_PUT_API = reqparse.RequestParser()
+SENSOR_ARGS_PUT_API.add_argument('X', type=float, help='X-axis.')
+SENSOR_ARGS_PUT_API.add_argument('Y', type=float, help='Y-axis.')
+SENSOR_ARGS_PUT_API.add_argument('Z', type=float, help='Z-axis.')
+
+MOTOR_ARGS_PUT_API = reqparse.RequestParser()
+MOTOR_ARGS_PUT_API.add_argument('SPEED', type=int, help='Speed of the Car.')
+MOTOR_ARGS_PUT_API.add_argument('A_RATE', type=float, help='Rate speed of motor A.')
+MOTOR_ARGS_PUT_API.add_argument('B_RATE', type=float, help='Rate speed of motor B.')
+
+
+LIGHT_ARGS_PUT_API = reqparse.RequestParser()
+LIGHT_ARGS_PUT_API.add_argument('HEAD', type=bool, help='Head light status.')
+LIGHT_ARGS_PUT_API.add_argument('LEFT', type=bool, help='Left light status.')
+LIGHT_ARGS_PUT_API.add_argument('RIGHT', type=bool, help='Right light status.')
+
+
 
 SYSTEM = {}
 LIGHT = {}
@@ -95,7 +122,7 @@ if __name__ == '__main__':
         SYSTEM = status['SYSTEM']
         MOTOR = status['MOTOR']
         SENSOR = status['SENSOR']
-        LIGHT = [status['LIGHT']]
+        LIGHT = status['LIGHT']
 
     with open('KEY.json', 'r') as f:
         KEY = json.load(f)
