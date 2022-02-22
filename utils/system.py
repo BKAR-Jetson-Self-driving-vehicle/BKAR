17c423826d2198af4416e8677b8a84b679b1157c
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-02-22 17:36:31 -0500

    update system module

--- utils/system.py
+++ utils/system.py
@@ -1,3 +1,35 @@
+#!./venv/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
++============================================================+
+- Author: Thanh HoangVan
+- School of Applied Mathematics and Informatics(SAMI of HUST)
+- Email: thanh.hoangvan051199@gmail.com
+- Github: https://github.com/thanhhoangvan
++============================================================+
+"""
+
 import time
 import threading
 import Jetson.GPIO as GPIO
+
+
+class Motor:
+    def __init__(self) -> None:
+        pass
+
+
+class Sensor:
+    def __init__(self) -> None:
+        pass
+
+
+class Light:
+    def __init__(self) -> None:
+        pass
+
+
+class System:
+    def __init__(self) -> None:
+        pass
