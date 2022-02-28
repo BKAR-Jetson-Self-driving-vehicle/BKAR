d2c6975c27b4ded00c5475a95733dbf3d9e89333
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-02-28 11:31:47 -0500

    completed Light module

--- utils/Light.py
+++ utils/Light.py
@@ -17,13 +17,22 @@ GPIO.setwarnings(False)
 
 
 class Light:
+    """
+    Điều khiển hệ thống đèn của BKAR qua GPIO
+    =========================================
+
+    Các tham số:
+    ---
+    - Chân kết nối pins: List tên các chân tương ứng với
+        các đèn [Head, Stop, Left, Right]
+    - Kiểu loại tên các chân kết nối: GPIO.BCM/GPIO.BOARD,
+    """
     def __init__(self, pins=[24, 23, 8, 7], mode=GPIO.BCM):
         self.Status = [0, 0, 0, 0]
 
         self.PINs = pins
-        self.mode = mode
 
-        GPIO.setmode(self.mode)
+        GPIO.setmode(mode)
         for pin in self.PINs:
             GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
 
