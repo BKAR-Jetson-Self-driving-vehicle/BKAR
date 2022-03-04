0d6c9067fcacf73fba625d90eae892fd0dc40ca0
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-03-04 10:10:31 -0500

    Script control Car by Controler/Gamepad

---
+++ LocalControl.py
@@ -0,0 +1,22 @@
+#!./venv/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
++============================================================+
+- Tác Giả: Hoàng Thành
+- Viện Toán Ứng dụng và Tin học(SAMI - HUST)
+- Email: thanh.hoangvan051199@gmail.com
+- Github: https://github.com/thanhhoangvan
++============================================================+
+"""
+
+import inputs
+import time
+
+
+def getControlerInput():
+    pass
+
+
+if __name__ == '__main__':
+    pass
