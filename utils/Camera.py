9fa4b1a3ddbf1e0752147b20b309f7271f8cbc26
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-04-01 11:42:48 +0700

    completed camera module

--- utils/Camera.py
+++ utils/Camera.py
@@ -8,6 +8,11 @@
 - Email: thanh.hoangvan051199@gmail.com
 - Github: https://github.com/thanhhoangvan
 +============================================================+
+
+https://github.com/JetsonHacksNano/CSI-Camera
+
+# Requirement:
+- Opencv with Gstreamer
 """
 import os
 import cv2
@@ -15,8 +20,38 @@ import time
 import threading
 
 
-class CSI_Camera:
+def gstreamer_pipeline(sensor_id=0,
+                       capture_width=1920, capture_height=1080,
+                       display_width=1920, display_height=1080,
+                       framerate=30, flip_method=0
+                       ):
+    """
+    Create a GStreamer pipeline for capturing from the CSI camera
+    ---
 
+    Parameter:
+    ---
+
+    Return
+    ---
+    """
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (sensor_id,
+           capture_width, capture_height,
+           framerate, flip_method,
+           display_width, display_height)
+    )
+
+
+class CSI_Camera:
+    """
+    """
     def __init__(self):
         self.video_capture = None
         self.frame = None
@@ -30,25 +65,35 @@ class CSI_Camera:
             self.video_capture = cv2.VideoCapture(
                 gstreamer_pipeline_string, cv2.CAP_GSTREAMER
             )
+            # Grab the first frame to start the video capturing
+            self.grabbed, self.frame = self.video_capture.read()
 
         except RuntimeError:
             self.video_capture = None
             print("Unable to open camera")
             print("Pipeline: " + gstreamer_pipeline_string)
-            return
-        self.grabbed, self.frame = self.video_capture.read()
 
     def start(self):
         if self.running:
             print('Video capturing is already running')
             return None
+        # create a thread to read the camera image
         if self.video_capture is not None:
             self.running = True
             self.read_thread = threading.Thread(target=self.updateCamera)
             self.read_thread.start()
         return self
 
+    def stop(self):
+        if self.running:
+            self.running = False
+        # Kill the thread
+        if self.read_thread is not None:
+            self.read_thread.join()
+            self.read_thread = None
+
     def updateCamera(self):
+        # This is the thread to read images from the camera
         while self.running:
             try:
                 grabbed, frame = self.video_capture.read()
@@ -57,6 +102,8 @@ class CSI_Camera:
                     self.frame = frame
             except RuntimeError:
                 print("Could not read image from camera")
+        # FIX ME - stop and cleanup thread
+        # Something bad happened
 
     def read(self):
         with self.read_lock:
@@ -68,129 +115,90 @@ class CSI_Camera:
         if self.video_capture is not None:
             self.video_capture.release()
             self.video_capture = None
+        # Now kill the thread
         if self.read_thread is not None:
             self.read_thread.join()
 
 
-class BKAR_Cameras:
+class Stereo_Camera:
     """
+    Read IMX219-83 Stereo Camera from Jetson Nano
     """
-    def __init__(self):
-        self.leftCam = CSI_Camera()
-        self.leftID = 0
-        self.leftFrame = None
+    def __init__(self, height=1080, width=1920, framerate=29, flip_mode=2):
+        self.height = height
+        self.width = width
+        self.FPS = framerate
+        self.flip = flip_mode
 
-        self.rightCam = CSI_Camera()
-        self.rightID = 1
-        self.rightFrame = None
+        self.lCam = CSI_Camera()
+        self.rCam = CSI_Camera()
 
-        self.BKARCam_threading = None
+        self.lFrame = None
+        self.rFrame = None
+
+        self.Status = False
+
+        self.thread_camera = threading.Thread(target=self.update, daemon=True)
         self.running = False
-        self.thread_lock = threading.Lock()
+        self.lock = threading.Lock()
 
-        self.Height = 1080
-        self.Width = 1920
-        self.FPS = 29
-        self.SensorMode = 2
-        self.FlipMethod = 2
+    def start(self):
+        self.lCam.open(gstreamer_pipeline(sensor_id=0, flip_method=self.flip,
+                                          capture_width=self.width,
+                                          capture_height=self.height,
+                                          display_width=self.width,
+                                          display_height=self.height))
+        self.rCam.open(gstreamer_pipeline(sensor_id=1, flip_method=self.flip,
+                                          capture_width=self.width,
+                                          capture_height=self.height,
+                                          display_width=self.width,
+                                          display_height=self.height))
 
-    def gstreamer_pipeline(
-        self,
-        sensor_id=0,
-        sensor_mode=2,
-        capture_width=1920,
-        capture_height=1080,
-        display_width=1920,
-        display_height=1080,
-        framerate=29,
-        flip_method=0
-    ):
-        return (
-            "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
-            "video/x-raw(memory:NVMM), "
-            "width=(int)%d, height=(int)%d, "
-            "format=(string)NV12, framerate=(fraction)%d/1 ! "
-            "nvvidconv flip-method=%d ! "
-            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx !"
-            "videoconvert ! "
-            "video/x-raw, format=(string)BGR ! appsink"
-            % (
-                sensor_id,
-                sensor_mode,
-                capture_width,
-                capture_height,
-                framerate,
-                flip_method,
-                display_width,
-                display_height,
-            )
-        )
-
-    def startBKARCameras(self):
-        """
-        """
-        # Start left Camera
-        self.leftCam.open(
-            self.gstreamer_pipeline(
-                sensor_id=self.leftID,
-                sensor_mode=self.SensorMode,
-                flip_method=self.FlipMethod,
-                display_height=self.Height,
-                display_width=self.Width
-            )
-        )
-        self.leftCam.start()
-
-        # Start right Camera
-        self.leftCam.open(
-            self.gstreamer_pipeline(
-                sensor_id=self.rightID,
-                sensor_mode=self.SensorMode,
-                flip_method=self.FlipMethod,
-                display_height=self.Height,
-                display_width=self.Width
-            )
-        )
-        self.rightCam.start()
-
-        # Start BKAR Cameras Thread
-        if self.running:
-            print("BKAR Cameras is running!")
-            return
-        if self.BKARCam_threading is not None:
+        time.sleep(2)
+        if not self.running:
             self.running = True
-            self.BKARCam_threading = threading.Thread(
-                target=self.captureCameras)
-            self.BKARCam_threading.setDaemon = True
-            self.BKARCam_threading.start()
+            self.thread_camera.start()
+        # wait for initialization
+        time.sleep(2)
 
-    def captureCameras(self):
+    def update(self):
+        """
+        Method auto capture from stereo camera
+            and save to self.lFrame, self.rFrame
+        """
         while self.running:
-            with self.thread_lock:
-                _, self.leftFrame = self.leftCam.read()
-                _, self.rightFrame = self.rightCam.read()
+            if self.rCam.video_capture.isOpened() and self.lCam.video_capture.isOpened():
+                try:
+                    ret1, self.lFrame = self.lCam.read()
+                    ret2, self.rFrame = self.rCam.read()
+                finally:
+                    self.Status = True
+            else:
+                self.Status = False
 
-    def getFrame(self):
-        """
-        """
-        return self.leftFrame, self.rightFrame
-
-    def stopBKARCameras(self):
-        self.running = False
-        self.BKARCam_threading.join
+    def getFrames(self):
+        return self.lFrame, self.rFrame
 
     def release(self):
-        self.running = False
-        if self.BKARCam_threading is not None:
-            self.BKARCam_threading.join()
-        self.leftCam.release()
-        self.rightCam.release()
+        if self.running:
+            self.running = False
+            time.sleep(0.1)
+            self.thread_camera.join()
+
+        self.lCam.stop()
+        self.lCam.release()
+
+        self.rCam.stop()
+        self.rCam.release()
+        time.sleep(1)
 
 
-if __name__ == '__main__':
-    bk_cam = BKAR_Cameras()
-    bk_cam.startBKARCameras()
-    time.sleep(1)
-    LF, RF = bk_cam.getFrame()
-    cv2.imwrite('camera.jpg', LF)
-    bk_cam.release()
+if __name__ == "__main__":
+    BCam = Stereo_Camera()
+    BCam.start()
+    time.sleep(2)
+    im1, im2 = BCam.getFrames()
+    cv2.imwrite("test1.jpg", im1)
+    cv2.imwrite("test2.jpg", im2)
+
+    BCam.release()
