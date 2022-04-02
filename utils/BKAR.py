bf23b45d74546b1b2a6db2b8a856727840a6ce77
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-04-02 16:29:59 +0700

    add author infor

--- utils/BKAR.py
+++ utils/BKAR.py
@@ -3,8 +3,8 @@
 
 """
 +============================================================+
-- Author: Thanh HoangVan
-- School of Applied Mathematics and Informatics(SAMI of HUST)
+- Tác Giả: Hoàng Thành
+- Viện Toán Ứng dụng và Tin học(SAMI - HUST)
 - Email: thanh.hoangvan051199@gmail.com
 - Github: https://github.com/thanhhoangvan
 +============================================================+
---
+++ utils/Control.py
@@ -0,0 +1,11 @@
+#!../venv/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
++============================================================+
+- Tác Giả: Hoàng Thành
+- Viện Toán Ứng dụng và Tin học(SAMI - HUST)
+- Email: thanh.hoangvan051199@gmail.com
+- Github: https://github.com/thanhhoangvan
++============================================================+
+"""
