5fc35de52df75a8287f342744f31047b4701c8b2
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-04-02 10:36:12 +0700

    update streamming method

--- utils/Server.py
+++ utils/Server.py
@@ -10,29 +10,96 @@
 +============================================================+
 """
 
+from __future__ import division
 import os
+import cv2
+import math
 import time
 import json
+import struct
+import socket
 import requests
 import threading
+import numpy as np
+
+
+"""
+Stream Video via UDP
+fork from: https://github.com/ancabilloni/udp_camera_streaming
+"""
+class FrameSegment(object):
+    """ 
+    Object to break down image frame segment
+    if the size of image exceed maximum datagram size 
+    """
+    MAX_DGRAM = 2**16
+    MAX_IMAGE_DGRAM = MAX_DGRAM - 64 # extract 64 bytes in case UDP frame overflown
+    def __init__(self, sock, port, addr="192.168.53.112"):
+        self.s = sock
+        self.port = port
+        self.addr = addr
+
+    def udp_frame(self, img):
+        """ 
+        Compress image and Break down
+        into data segments 
+        """
+        compress_img = cv2.imencode('.jpg', img)[1]
+        dat = compress_img.tobytes()
+        size = len(dat)
+        count = math.ceil(size/(self.MAX_IMAGE_DGRAM))
+        array_pos_start = 0
+        while count:
+            array_pos_end = min(size, array_pos_start + self.MAX_IMAGE_DGRAM)
+            self.s.sendto(struct.pack("B", count) +
+                dat[array_pos_start:array_pos_end], 
+                (self.addr, self.port)
+                )
+            array_pos_start = array_pos_end
+            count -= 1
 
 
 class ConnectServer:
     """
     """
     def __init__(self,
-                 ServerIP='192.168.53.111',
-                 ServerPort=5000):
+                 ServerIP='192.168.53.102',
+                 ServerPort=5000,
+                 StereoCamInstance=None,,
+                 StreamPort=8000):
+        self.MyIP = '192.168.0.0'
         self.ServerIP = ServerIP
         self.PORT = ServerPort
         self.ConnectStatus = False
 
-        self.locking = threading.Lock()
-        self.server_thread = None
+        self.StereoCamera = StereoCamInstance
+        self.StreamPort = StreamPort
+
+        self.serverLock = threading.Lock()
+        self.serverThread = None
         self.running = False
+        
+        self.videoStream = None
+        self.videoThread = None
+        self.videoLock = None
+        self.streamming = False
 
     # ======================================
     def start(self):
+        if self.running == False:
+            self.serverThread = threading.Thread(target=self.checkConnection, daemon=True)
+            self.running = True
+            time.sleep(0.5)
+            self.serverThread.start()
+        
+        if self.streamming == False:
+            self.videoStream = threading.Thread(target=self.streamVideo, daemon=True)
+            self.streamming = True
+            time.sleep(1.5)
+            self.streamming = True
+
+    # ======================================
+    def checkConnection(self):
         pass
 
     # ======================================
@@ -78,7 +145,7 @@ class ConnectServer:
         url_api = 'http://' + self.ServerIP + ':' + str(self.PORT) + '/System'
         headers = headers = {'content-type': 'application/json'}
         data = {"TIMESTAMP": time.time(),
-                "IP": self.IP,
+                "IP": self.MyIP,
                 "CONNECTED": self.ConnectStatus,
                 "DISTANCE": Distance,
                 "VOLTAGE": Voltage,
@@ -90,8 +157,24 @@ class ConnectServer:
                                 headers=headers)
         return response.status_code
 
+    # ======================================
     def streamVideo(self):
-        pass
+        """
+        Streamming Video via UDP
+        """
+        # Set up UDP socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        port = self.StreamPort
+        address = self.ServerIP
+
+        fs = FrameSegment(s, port, addr=address)
+
+        while self.streamming:
+            frame1, frame2 = self.StereoCamera.read()
+            if frame1 is not None:
+            fs.udp_frame(frame1)
+
+        s.close()
 
     # ======================================
     def getControl(self):
