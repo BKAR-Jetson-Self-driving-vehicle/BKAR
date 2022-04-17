6ce1aa23c40a0e4e1a4159d01db507c4a9fc008b
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-04-17 22:00:42 +0700

    small update

--- utils/core/Light.py
+++ utils/core/Light.py
@@ -120,11 +120,12 @@ class Light:
 
 if __name__=='__main__':
     Lg = Light()
-    for i in range(5):
+    for i in range(10):
         for ID in range(4):
             Lg.turnOn(ID)
-            time.sleep(0.1)
+        time.sleep(2)
+        for ID in range(4):
             Lg.turnOff(ID)
-            time.sleep(0.1)
+        time.sleep(2)
     Lg.turnOffAll()
     GPIO.cleanup()
