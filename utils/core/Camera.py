c905ef58e5091516c8ae9dfb68dc82114b19eec6
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-05-01 03:05:58 +0700

    complete Camera.py module

--- utils/core/Camera.py
+++ utils/core/Camera.py
@@ -15,11 +15,103 @@
 """
 import os
 import time
+import threading
 
 import cv2
 import numpy as np
-from multiprocessing import Queue, Value, Process, Lock
-from jetcam.csi_camera import CSICamera
+from multiprocessing import Queue, Value, Process
+
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=200,
+    display_height=150,
+    framerate=30,
+    flip_method=0):
+    
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (sensor_id, capture_width, capture_height, framerate, flip_method, display_width, display_height))
+
+
+class CSI_Camera:
+
+    def __init__(self):
+        # Initialize instance variables
+        # OpenCV video capture element
+        self.video_capture = None
+        # The last captured image from the camera
+        self.frame = None
+        self.grabbed = False
+        # The thread where the video capture runs
+        self.read_thread = None
+        self.read_lock = threading.Lock()
+        self.running = False
+
+    def open(self, gstreamer_pipeline_string):
+        try:
+            self.video_capture = cv2.VideoCapture(
+                gstreamer_pipeline_string, cv2.CAP_GSTREAMER
+            )
+            # Grab the first frame to start the video capturing
+            self.grabbed, self.frame = self.video_capture.read()
+
+        except RuntimeError:
+            self.video_capture = None
+            print("Unable to open camera")
+            print("Pipeline: " + gstreamer_pipeline_string)
+
+
+    def start(self):
+        if self.running:
+            print('Video capturing is already running')
+            return None
+        # create a thread to read the camera image
+        if self.video_capture != None:
+            self.running = True
+            self.read_thread = threading.Thread(target=self.updateCamera)
+            self.read_thread.start()
+        return self
+
+    def stop(self):
+        self.running = False
+        # Kill the thread
+        self.read_thread.join()
+        self.read_thread = None
+
+    def updateCamera(self):
+        # This is the thread to read images from the camera
+        while self.running:
+            try:
+                grabbed, frame = self.video_capture.read()
+                with self.read_lock:
+                    self.grabbed = grabbed
+                    self.frame = frame
+            except RuntimeError:
+                print("Could not read image from camera")
+        # FIX ME - stop and cleanup thread
+        # Something bad happened
+
+    def read(self):
+        with self.read_lock:
+            frame = self.frame.copy()
+            grabbed = self.grabbed
+        return grabbed, frame
+
+    def release(self):
+        if self.video_capture != None:
+            self.video_capture.release()
+            self.video_capture = None
+        # Now kill the thread
+        if self.read_thread != None:
+            self.read_thread.join()
 
 
 class Stereo_Camera:
@@ -27,53 +119,67 @@ class Stereo_Camera:
     Read IMX219-83 Stereo Camera from Jetson Nano
     """
     def __init__(self, camera_configs={},
-                 mp_queue0=None, mp_queue1=None,
-                 mp_running=None):
+                 mp_queue0=None, mp_queue1=None):
 
         self.CAM_CONFIGS = camera_configs
 
         self.mp_queues = [mp_queue0, mp_queue1]
         self.CaptureProcess = None
-        self.mp_running = mp_running
+        self.mp_running = Value("I", 0)
 
     def start(self):
-        self.CaptureProcess = Process(target=self.Capturing,
-                                      args=(self.CAM_CONFIGS, self.mp_queues, self.mp_running))
+        self.mp_running.value = 1
+        self.CaptureProcess = Process(target=self.Capturing, args=(self.CAM_CONFIGS, self.mp_queues, self.mp_running))
         self.CaptureProcess.start()
         print('start successful!')
 
     def Capturing(self, CONFIGS, Queues, running):
-        left_cam = CSICamera(capture_device=0,
-                         width=CONFIGS['width'], height=CONFIGS['height'],
-                         capture_width=CONFIGS['width'], capture_height=CONFIGS['height'],
-                         capture_fps=CONFIGS['fps'], capture_flip=CONFIGS['flip'])
+        left_camera = CSI_Camera()
+        right_camera = CSI_Camera()
         
-        right_cam = CSICamera(capture_device=1,
-                         width=CONFIGS['width'], height=CONFIGS['height'],
-                         capture_width=CONFIGS['width'], capture_height=CONFIGS['height'],
-                         capture_fps=CONFIGS['fps'], capture_flip=CONFIGS['flip'])
+        left_camera.open(
+            gstreamer_pipeline(
+                sensor_id=0,
+                capture_width=CONFIGS['width'],
+                capture_height=CONFIGS['height'],
+                flip_method=CONFIGS['flip'],
+                display_width=640,
+                display_height=380,
+            )
+        )
+        right_camera.open(
+            gstreamer_pipeline(
+                sensor_id=1,
+                capture_width=CONFIGS['width'],
+                capture_height=CONFIGS['height'],
+                flip_method=CONFIGS['flip'],
+                display_width=640,
+                display_height=380,
+            )
+        )
 
-        while running.value == 1:
-            lframe = left_cam.read()
-            rframe = right_cam.read()
-            
-            if not Queues[0].full():
-                Queues[0].put(lframe)
-            if not Queues[0].full():
-                Queues[1].put(rframe)
+        left_camera.start()
+        right_camera.start()
 
-        self.stop()
+        if left_camera.video_capture.isOpened() and right_camera.video_capture.isOpened():
+            while True:
+                _, left_image = left_camera.read()
+                _, right_image = right_camera.read()
+                
+                if not Queues[0].full():
+                    Queues[0].put(left_image)
+                if not Queues[1].full():
+                    Queues[1].put(right_image)
 
-    def getFrames(self):
-        pass
+                if running.value == 0:
+                    break
+        
+        left_camera.stop()
+        left_camera.release()
+        right_camera.stop()
+        right_camera.release()
 
-    def saveFrames(self):
-        pass
-
-    def showCameras(self):
-        pass
-
-    def stop(self):
+    def cleanQueues(self):
         print('Cleanning queues')
         while True:
             if not self.mp_queues[0].empty():
@@ -84,17 +190,21 @@ class Stereo_Camera:
                 break
         print('Cleaned!')
 
-        # Error can not join process because waitting something, fix it later
+    def stop(self):
+        self.mp_running.value = 0
+        time.sleep(1)
+        self.cleanQueues()
+        
         self.CaptureProcess.join()
         print('process Capturing is stopped!')
 
 
 if __name__ == "__main__":
 
-    queue_camera_0 = Queue(maxsize=200)
-    queue_camera_1 = Queue(maxsize=200)
+    # Set maxsize small because, if it bigger, it can store more frame and delay to current time
+    queue_camera_0 = Queue(maxsize=3)
+    queue_camera_1 = Queue(maxsize=3)
 
-    mp_running = Value("I", 1)
     config = {
         'height': 720,
         'width': 1280,
@@ -102,7 +212,7 @@ if __name__ == "__main__":
         'flip': 2,
         }
     
-    MY_CAM = Stereo_Camera(config, queue_camera_0, queue_camera_1, mp_running)
+    MY_CAM = Stereo_Camera(config, queue_camera_0, queue_camera_1)
     MY_CAM.start()
     start_time =time.time()
     
@@ -113,14 +223,17 @@ if __name__ == "__main__":
         if not queue_camera_1.empty():
             cam1 = queue_camera_1.get()
 
-        # cv2.imwrite('data/' + str(time.time()) + '.jpg', cam0)
+        if cam0 is not None:
+            cv2.imshow("CSI Camera", cam0)
 
-        if int(time.time()-start_time) > 5:
+        if cv2.waitKey(25)==ord('q'):
             break
 
-    mp_running.value = 0
+        if int(time.time()-start_time) > 20:
+            break
+
+    cv2.destroyAllWindows()
+    # mp_running.value = 0
+    # time.sleep(2)
+    MY_CAM.stop()
     print('Stopping program!')
-    # time.sleep(3)
-    # cv2.destroyAllWindows()
-    # MY_CAM.stop()
-    # print('stop successful!')
